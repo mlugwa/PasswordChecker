3789d748fe1d95fa1e7b1e0433fc48ef87214672
Mlungisi <mgmgwala@gmail.com>
2019-09-10 15:04:35 +0200

    ...

--- Jasmine/spec/ValidateSpec.js
+++ Jasmine/spec/ValidateSpec.js
@@ -1,18 +1,20 @@
 describe("Validate Password", function(){
-	var password = "Ffggg88888";
-	it("should be at least 8 characters long",function(){
-		expect(password_is_valid(password)).toBe("password is valid");
+	var password = "Ffg";
+	it("should not be passed a password shorter than 8 characters",function(){
+		expect(password_is_valid(password)).toBe("password is too short");
 	});
-	it("should not be null",function(){
-		expect(password_is_valid(password)).toBe("password is valid");
+	password = "";
+	it("should throw an Error if password is empty",function(){
+		expect(password_is_valid(password)).toBe("password cannot be empty");
 	});
+	password = "Ffggg888888";
 	it("should contain a lowercase letter",function(){
 		expect(password_is_valid(password)).toBe("password is valid");
 			});
 	it("should contain a uppercase letter",function(){
 		expect(password_is_valid(password)).toBe("password is valid");
 	});
-	it("should contain a lowercase letter",function(){
+	it("should contain a number",function(){
 		expect(password_is_valid(password)).toBe("password is valid");
 });
 
